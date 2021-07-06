957a5d656aa171863dd45497b1e9dc84d9cb1676
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-06 12:39:24 +0530

    letter support

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -2,10 +2,15 @@ const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const numbers = '0123456789'
 const symbols = '!@#$%^&*()-_=+[{]}\\|;:\'"<,>.?/'
 
-const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
-	let chars = alpha;
+const createPassword = (length = 8, hasNumbers = true, hasSymbols = true, hasLetters = true) => {
+	let chars = '';
+	hasLetters ? (chars += alpha) : ''
 	hasNumbers ? (chars += numbers) : ''
 	hasSymbols ? (chars += symbols) : ''
+
+	if (chars.length === 0) {
+		return 'What kind of password you want, an empty one ??'
+	}
 
 	return generatePassword(length, chars);
 }
