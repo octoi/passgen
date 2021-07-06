b07ec6325c05fd939f319c52c51eb86863693edf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-06 12:22:40 +0530

    options

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const program = require('commander');
 
 program.version('1.0.0').description('Simple password generator'); // version & description of app
+
+program
+	.option('-l, --length <number>', 'length of password', '8')
+	.option('-s, --save', 'save password to password.txt', false)
+	.option('-nn, --no-numbers', 'remove numbers')
+	.option('-ns, --no-symbols', 'remove symbols')
+	.parse()
